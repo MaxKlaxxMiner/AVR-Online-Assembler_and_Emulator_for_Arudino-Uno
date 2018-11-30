dd05f334a8863438cc1f431bb21eaad748368b89
MaxKlaxx <max@tetrisk.de>
2018-11-30 22:45:01 +0100

    autoformat + linebreak: CRLF

--- AvrAssembler/app.ts
+++ AvrAssembler/app.ts
@@ -1,28 +1,33 @@
-﻿class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
+﻿class Greeter
+{
+  element: HTMLElement;
+  span: HTMLElement;
+  timerToken: number;
 
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
-    }
+  constructor(element: HTMLElement)
+  {
+    this.element = element;
+    this.element.innerHTML += "The time is: ";
+    this.span = document.createElement("span");
+    this.element.appendChild(this.span);
+    this.span.innerText = new Date().toUTCString();
+  }
 
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
-    }
+  start()
+  {
+    this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
+  }
 
-    stop() {
-        clearTimeout(this.timerToken);
-    }
+  stop()
+  {
+    clearTimeout(this.timerToken);
+  }
 
 }
 
-window.onload = () => {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
+window.onload = () =>
+{
+  var el = document.getElementById("content");
+  var greeter = new Greeter(el);
+  greeter.start();
 };
